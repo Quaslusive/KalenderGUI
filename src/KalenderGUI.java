27f6b432fc44ef0b66a1503d463367d674413650
Joel Bech <122796634+JoBch@users.noreply.github.com>
2023-12-07 14:29:14 +0100

    Small fixes, comments

--- src/KalenderGUI.java
+++ src/KalenderGUI.java
@@ -35,25 +35,24 @@ public class KalenderGUI extends JFrame {
             //Adding every day in their own JPanel and adding a border to better accentuate the days.
             JPanel dayPanel = new JPanel(new BorderLayout());
             dayPanel.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+
             //Adding a second panel to the bottom of the day and adding my buttons to it to further make the calendar more readable.
             JPanel bottomPanel = new JPanel(new GridLayout(2, 0));
 
             LocalDate day = startOfWeek.plusDays(i); // Incrementer en dag
             dayLabels[i] = new JLabel(String.valueOf(day)); // Lägger in datum på elementen
-            if (day.equals(today)) { // Om en dag == med dagens datum, blir texten röd
-                Font fontCurrentDag = new Font(Font.SANS_SERIF, Font.BOLD, 20);
-                //dayLabels displays the corresponding days date
-                dayLabels[i].setFont(fontCurrentDag);
+            if (day.equals(today))  // Om en dag == med dagens datum, blir texten röd
                 dayLabels[i].setForeground(Color.RED);
-            }
-            //Maybe some more comment here about what does what?
+
             //dayTexts displays your activity
             dayTexts[i] = new JTextArea();
             dayTexts[i].setEditable(false);
+
             //Button which opens a JOptionPane that lets you insert an activity
-            addButtons[i] = new JButton("Lägg till event");
             byte finalI = (byte) i;
+            addButtons[i] = new JButton("Lägg till event");
             addButtons[i].addActionListener(e -> dayTexts[finalI].setText(JOptionPane.showInputDialog("Skriv in din event :) ")));
+
             //Buttons to clear events form your dayTexts
             clearButtons[i] = new JButton("Ta bort event");
             clearButtons[i].addActionListener(e -> dayTexts[finalI].setText(""));
